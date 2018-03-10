e3a3cc704a63526f6d6d4823e5eb0b6c5afa1bae
Rebecca <rxs6737@rit.edu>
2018-03-09 22:35:16 -0500

    changed lock script

--- i3/.config/i3/lock.sh
+++ i3/.config/i3/lock.sh
@@ -1,4 +1,17 @@
 imagePath=~/Pictures/wallpapers/smallMemories2.png
 
 i3lock -i $imagePath \
-	--clock
+	--clock \
+	--timesize=58 \
+	--timecolor=ffffffff \
+	--timepos=w-325:h-150 \
+	--datestr=%D \
+	--datesize=12 \
+	--datecolor=ffffffff \
+	--datepos=tx+85:ty+25 \
+	--indicator \
+	--ringcolor=ffffff22 \
+	--insidecolor=ffffff11 \
+	--radius=30 \
+	--textsize=12 \
+	--indpos=40:h-40 \
